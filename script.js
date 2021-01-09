4e5bbce3878c26d4e5b1404e4f997130d671462c
oLORDer <justsendmeow@gmail.com>
2021-01-09 17:16:41 +0100

    Function for click mouth

---
+++ script.js
@@ -0,0 +1,38 @@
+let mouth = document.getElementById('mouth');
+let eyeActive = document.getElementsByClassName('eyeActive');
+
+let eY = fasle;
+mouth.onclick = function () {
+    eY = true;
+    if (eY == true) {
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const theBag = document.getElementById('bag');
+// theBag.addEventListener('click', questOf);
+
+// let answ;
+// function questOf() {
+//     while (answ !== 3) {
+//         alert('try again');
+//         answ = prompt('number from 1 to 5');
+//     } alert('you are right');
+// }
+
+// theBag.onclick = function () {
+//     this.classList.toggle('activeCl');
+// }
